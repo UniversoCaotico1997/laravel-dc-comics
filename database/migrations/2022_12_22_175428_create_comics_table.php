035994f5acb6e0c6ff204cd6dffdd476d5fade20
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-12-22 20:05:59 +0100

    Create/Complete Seeder + push

--- database/migrations/2022_12_22_175428_create_comics_table.php
+++ database/migrations/2022_12_22_175428_create_comics_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->text('description');
+            $table->string('thumb');
+            $table->float('price', 4, 2);
+            $table->string('series', 50);
+            $table->date('sale_date', 15);
+            $table->string('type', 20);
             $table->timestamps();
         });
     }
