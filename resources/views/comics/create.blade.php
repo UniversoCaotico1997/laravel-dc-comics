d694d2e63f59718209f79bedcec1c834a0a01732
UniversoCaotico1997 <loria.roberto97@gmail.com>
2023-01-08 17:06:08 +0100

    refactoring create.blade

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -3,28 +3,41 @@
 @section('content')
 
 <div class="container">
-    <form action="" method="post">
-        <div class="form-group">
-            <label for="" class="text-white">Title</label>
-            <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
-
-            <label for="" class="text-white">Description</label>
-            <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
-
-            <label for="" class="text-white">Thumb</label>
-            <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
-
-            <label for="" class="text-white">Price</label>
-            <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
-
-            <label for="" class="text-white">Series</label>
-            <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
-
-            <label for="" class="text-white">Sale_date</label>
-            <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
-
-            <label for="" class="text-white">Type</label>
-            <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+    <form action="{{route('comics.store')}}" method="post">
+        @csrf
+        <div class="mb-3">
+            <label for="title" class="form-label text-white">Title</label>
+            <input type="text" name="title" id="title" class="form-control" placeholder="Comics Title" aria-describedby="helpId">
+            <small id="helpTitle" class="text-muted">Add Title</small>
+        </div>
+        <div class="mb-3">
+            <label for="description" class="form-label text-white">Description</label>
+            <textarea class="form-control" name="description" id="description" rows="4"></textarea>
+        </div>
+        <div class="mb-3">
+            <label for="src" class="form-label text-white">Thumb Comics</label>
+            <input type="text" name="src" id="src" class="form-control" placeholder="Comics" aria-describedby="helpId">
+            <small id="helpThumb" class="text-muted">Add Thumb Comics</small>
+        </div>
+        <div class="mb-3">
+            <label for="price" class="form-label text-white">Price</label>
+            <input type="text" name="Price" id="Price" class="form-control" placeholder="Comics" aria-describedby="helpId">
+            <small id="helpPrice" class="text-muted">Add Price</small>
+        </div>
+        <div class="mb-3">
+            <label for="series" class="form-label text-white">Series</label>
+            <input type="text" name="series" id="series" class="form-control" placeholder="Comics" aria-describedby="helpId">
+            <small id="helpSeries" class="text-muted">Add Series</small>
+        </div>
+        <div class="mb-3">
+            <label for="sale_date" class="form-label text-white">Sale_Date</label>
+            <input type="text" name="sale_date" id="sale_date" class="form-control" placeholder="Comics" aria-describedby="helpId">
+            <small id="helpSale_Date" class="text-muted">Add Sale_Date</small>
+        </div>
+        <div class="mb-3">
+            <label for="type" class="form-label text-white">Type</label>
+            <input type="text" name="type" id="type" class="form-control" placeholder="Comics" aria-describedby="helpId">
+            <small id="helpType" class="text-muted">Add Type</small>
         </div>
         <div>
             <a href="" class="btn btn-primary my-3">Send</a>
